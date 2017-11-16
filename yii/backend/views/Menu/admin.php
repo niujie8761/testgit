d66c2ec2b59d3344919e31d5bd17ceb5e81b47b5
niujie <1033751979@qq.com>
2017-11-16 10:00:31 +0800

    提交admin.php

--- yii/backend/views/Menu/admin.php
+++ yii/backend/views/Menu/admin.php
@@ -1,10 +1,12 @@
 <link type="text/css" rel="stylesheet" href="/backend/web/css/365baseAdmin.css">
+<link type="text/css" rel="stylesheet" href="http://pic.house365.com/erbu/fgjadmin/css/365baseAdmin.css">
+<link rel="stylesheet" href="/backend/web/css/boxy.css" type="text/css" />
 <script src="http://pic.house365.com/static/jquery/jquery-1.11.1.min.js"></script>
 <div class="pageTitle">
     <div class="title">后台菜单权限管理</div>
 </div>
 <ul class="optionList">
-    <li><a href="#" onclick="fill_menu_form(-1);return false;"><span class="icon-add"></span>添加权限菜单</a></li>
+    <li><a href="javascript:void(0);" class="auth"><span class="icon-add"></span>添加权限菜单</a></li>
 </ul>
 <table width="100%" class="tableList">
     <tr>
@@ -16,13 +18,13 @@
         <th>操作</th>
     </tr>
     <?php foreach ($menus as $key => $_menu) : ?>
-        <tr>
+        <tr <?php if($key%2 == 0) {echo "class='odd'";}else{echo "class='even'";}?>>
             <td><?=$_menu['kamu_id']?></td>
             <td style="padding-left:<?=($_menu['level_depth']-1)*25?>px;"><?=$_menu['kamu_name']?></td>
             <td><?=$_menu['kamu_url']?></td>
             <td><?=$_menu['kamu_as_menu']==1?'<font color="Green">是</font>':'<font color="Red">否</font>'?></td>
             <td><?=$_menu['position']?></td>
-            <td><a class="edit" href="#" onclick="fill_menu_form(<?=$key?>);return false;">编辑</a> | <a class="delete" href="/menu/admin_delete?id=<?=$_menu['kamu_id']?>">删除</a></td>
+            <td><a class="edit" href="javascript:void(0);">编辑</a> | <a class="delete" href="/menu/admin_delete?id=<?=$_menu['kamu_id']?>">删除</a></td>
         </tr>
     <?php endforeach; ?>
 </table>
@@ -33,19 +35,29 @@
 <div id="J_add_menu" style="display:none;">
     <span class="close">关闭</span>
     <br>
-    <form method="post" action="<?php echo Yii::$app->urlManager->createUrl(['menu/save'])?>" >
+    <form method="post" action="<?php echo Yii::$app->urlManager->createUrl(['menu/add'])?>" >
         <table class="tableForm" width="100%">
             <tr>
-                <th style="width:25%;">菜单名称：</th><td><input class="text" name="kamu_name" id="menu_name" value=""></td>
+                <th style="width:25%;">菜单名称：</th>
+                <td>
+                    <input class="text" name="kamu_name" id="menu_name" value="">
+                </td>
             </tr>
             <tr>
-                <th style="width:25%;">菜单地址：</th><td><input class="textBig" name="kamu_url" id="menu_url" value=""></td>
+                <th style="width:25%;">菜单地址：</th>
+                <td>
+                    <input class="textBig" name="kamu_url" id="menu_url" value="">
+                </td>
             </tr>
             <tr>
-                <th style="width:25%;">排序：</th><td><input class="textSmall" name="position" id="menu_position" value="">数值大的在前面</td>
+                <th style="width:25%;">排序：</th>
+                <td>
+                    <input class="textSmall" name="position" id="menu_position" value="">数值大的在前面
+                </td>
             </tr>
             <tr>
-                <th style="width:25%;">父集菜单：</th><td>
+                <th style="width:25%;">父集菜单：</th>
+                <td>
                     <select class="select" name="kamu_parent_id" id="menu_parent">
                         <option value="0">作为父菜单使用</option>
                         <?php foreach ($top_menus as $_menu) : ?>
@@ -55,7 +67,8 @@
                 </td>
             </tr>
             <tr>
-                <th style="width:25%;">是否作为菜单：</th><td>
+                <th style="width:25%;">是否作为菜单：</th>
+                <td>
                     <select class="select" name="kamu_as_menu" id="menu_as">
                         <option value="1">是</option>
                         <option value="0">否</option>
@@ -63,8 +76,9 @@
                 </td>
             </tr>
             <tr>
-                <th></th><td>
-                    <input type="hidden" name="kamu_id" id="menu_id" value="0">
+                <th></th>
+                <td>
+                    <input type="hidden" value="0" name="kamu_id" id="kamu_id"/>
                     <input type="submit" class="submit" value="提交">
                 </td>
             </tr>
@@ -72,6 +86,54 @@
     </form>
 </div>
 <script>
+    $(document).ready(function(){
+
+        //添加权限菜单
+       $('.auth').on('click', function() {
+           var height = $(window).height();
+           var width = $(window).width();
+           var top = (height-300)/2+'px';
+           var left = (width-600)/2+'px';
+           $("#J_add_menu").css({top: top, left: left});
+           $("#J_add_menu").show();
+
+       });
+        //编辑权限菜单
+        $('.edit').on('click', function() {
+            var height = $(window).height();
+            var width = $(window).width();
+            var top = (height-300)/2+'px';
+            var left = (width-600)/2+'px';
+           var kamu_id = $(this).parents('tr').children('td').eq(0).html();
+           $.ajax({
+               'url': '<?= Yii::$app->urlManager->createUrl('menu/save')?>',
+               'type' : 'post',
+               'dataType' : 'json',
+               'async' : 'false',
+               'data' : {kamu_id: kamu_id},
+               'success' : function(result) {
+                        $("#menu_name").val(result.kamu_name);
+                        $("#menu_url").val(result.kamu_url);
+                        $("#menu_position").val(result.position);
+                        $("#menu_parent option").each(function() {
+                           if($(this).val() == result.kamu_parent_id) {
+                               $(this).prop("selected", true);
+                           }
+                        });
+                        $("#menu_as option").each(function() {
+                           if($(this).val() == result.kamu_as_menu) {
+                               $(this).prop("selected", true);
+                           }
+                        });
+                        $("#kamu_id").val(result.kamu_id);
+                        $("#J_add_menu").css({top: top, left: left});
+                        $("#J_add_menu").show();
+               }
+           });
+
+        });
+
+    });
     var menus = new Array();
     <?php foreach ($menus as $key => $val):?>
     menus['<?=$key?>'] = new Array();
@@ -82,7 +144,7 @@
     <?php endforeach;?>
 
     //表单数据JS填充
-    function fill_menu_form(key){
+   /* function fill_menu_form(key){
         //变量赋值
         var menu_name = '';
         var menu_url = '';
@@ -113,10 +175,10 @@
         $('#J_add_menu').css({top:top+'px',left:left+'px'});
         $('#J_add_menu').show();
         return false;
-    }
-    /*$(function(){
+    }*/
+    $(function(){
         $('#J_add_menu .close').click(function(){
             $('#J_add_menu').hide();
         });
-    });*/
+    });
 </script>
